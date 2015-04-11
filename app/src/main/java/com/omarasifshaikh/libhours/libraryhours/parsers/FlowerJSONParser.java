845002db15c8def49c3255902cbb48c3b6588023
Omar Shaikh <omarasifshaikh@hotmail.com>
2015-04-10 21:18:02 -0700

    auto

--- app/src/main/java/com/omarasifshaikh/libhours/libraryhours/parsers/FlowerJSONParser.java
+++ app/src/main/java/com/omarasifshaikh/libhours/libraryhours/parsers/FlowerJSONParser.java
@@ -1,8 +1,14 @@
 package com.omarasifshaikh.libhours.libraryhours.parsers;
 
+import com.omarasifshaikh.libhours.libraryhours.model.Flower;
+
+import java.util.List;
+
 /**
  * Created by Omar on 4/10/2015.
  */
 public class FlowerJSONParser {
+    //public static List<Flower> parseFeed(String content){
 
+    //}
 }
